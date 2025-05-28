83cfc52d41584ca2df654e0c463f2c6fa8c3762f
Rich Felker <dalias@aerifal.cx>
2025-05-28 11:04:32 -0400

    flesh out, comment, and get nfd interator to the point of compiling

--- nfd.c
+++ nfd.c
@@ -1,5 +1,6 @@
-
-
+#include <stdlib.h>
+#include <limits.h>
+#include <wchar.h>
 
 struct decomp_iterator {
 	const char *src;
@@ -8,15 +9,16 @@ struct decomp_iterator {
 };
 
 struct ccc_iterator {
-	struct decomp_iterator di, di_start, di_min;
-	
+	struct decomp_iterator di, di_start;
+	int cur_ccc;
 };
 
-
+int cccof(wchar_t);
+const void *find_decomp(wchar_t);
 
 wchar_t decomp_iterate(struct decomp_iterator *di)
 {
-	if (!*di->cur) {
+	if (!di->cur) {
 		wchar_t wc;
 		int l = mbtowc(&wc, di->src, MB_LEN_MAX);
 		if (l < 0) {
@@ -26,21 +28,25 @@ wchar_t decomp_iterate(struct decomp_iterator *di)
 		di->src += l;
 		di->cur = find_decomp(wc);
 		di->pos = 0;
+		if (!di->cur) return wc;
 	}
 }
 
-
+void decomp_iterator_start(struct decomp_iterator *di, const char *src)
+{
+	di->src = src;
+	di->cur = 0;
+	di->pos = 0;
+}
 
 wchar_t ccc_iterate(struct ccc_iterator *ci)
 {
 	wchar_t wc;
-	if (!in progress) {
-		wc = decomp_iterate(&ci->di);
-		ccc = cccof(wc);
-		if (!ccc) return wc;
-		ci->di_start = ci->di;
-		ci->cur_ccc = 0;
-	}
+	int ccc;
+	/* If there's already a sequence of non-starters where we have
+	 * found the desired next ccc in a previous step, iterate characters
+	 * until we find the next match with the same ccc or reach the next
+	 * starter. */
 	if (ci->cur_ccc) {
 		do {
 			wc = decomp_iterate(&ci->di);
@@ -48,16 +54,20 @@ wchar_t ccc_iterate(struct ccc_iterator *ci)
 			if (ccc == ci->cur_ccc) return wc;
 		} while (ccc);
 	}
-	next_ccc = ci->cur_ccc + 1;
+	/* Start or resume processing at the decomposition iterator position
+	 * just after the last starter, and look for the lowest-ccc character
+	 * with ccc at least 1 plus the previously-current ccc. */
 	ci->di = ci->di_start;
+	int next_ccc = ci->cur_ccc + 1;
 	int lowest_greater_ccc = 256;
 	wchar_t lowest_ccc_wc = WEOF;
+	struct decomp_iterator lowest_ccc_di;
 	do {
 		wc = decomp_iterate(&ci->di);
 		ccc = cccof(wc);
 		if (ccc >= next_ccc && ccc < lowest_greater_ccc) {
 			lowest_greater_ccc = ccc;
-			ci->di_min = di;
+			lowest_ccc_di = ci->di;
 			lowest_ccc_wc = wc;
 		}
 	} while (ccc > next_ccc);
@@ -71,5 +81,14 @@ wchar_t ccc_iterate(struct ccc_iterator *ci)
 		ci->cur_ccc = 0;
 		return wc;
 	}
-	return lowest_wc;
+	ci->di = lowest_ccc_di;
+	ci->cur_ccc = lowest_greater_ccc;
+	return lowest_ccc_wc;
+}
+
+void ccc_iterator_start(struct ccc_iterator *ci, const char *src)
+{
+	decomp_iterator_start(&ci->di_start, src);
+	ci->di = ci->di_start;
+	ci->cur_ccc = 0;
 }
